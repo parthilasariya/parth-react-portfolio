749f943b7f0a55343541e5e13519a6fbf3e9a368
Parth Ilasariya <parth@parths-mbp.home>
2026-08-17 20:47:52 -0300

    Finalize portfolio fixes and build config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images:{
-    loader:'akamai',
-    path:''
-  }
-  
-}
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
